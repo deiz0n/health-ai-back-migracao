4470a04d819712ae7450324f236c9847bfca715f
deiz0n <eduardomacedosilva@outlook.com>
2026-03-23 17:10:55 -0300

    feat: MedicoModel

---
+++ src/domain/MedicoModel.ts
@@ -0,0 +1,10 @@
+export class MedicoModel {
+  constructor(
+    public nome: string,
+    public sobrenome: string,
+    public email: string,
+    public senha: string,
+    public crm: string,
+    public cpf: string,
+  ) {}
+}
