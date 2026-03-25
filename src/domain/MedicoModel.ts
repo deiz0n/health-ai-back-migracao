5e616a90af8d0e77e4439423c9acae2be77a840c
deiz0n <eduardomacedosilva@outlook.com>
2026-03-25 00:15:46 -0300

    refactor: adição do atributo id, createdAt e updatedAt e os campos de senha e cpf se tornaram opcionais

--- src/domain/MedicoModel.ts
+++ src/domain/MedicoModel.ts
@@ -1,10 +1,13 @@
 export class MedicoModel {
   constructor(
+    public id: string,
     public nome: string,
     public sobrenome: string,
-    public email: string,
-    public senha: string,
     public crm: string,
-    public cpf: string,
+    public email: string,
+    public senha?: string,
+    public cpf?: string,
+    public createdAt?: Date,
+    public updatedAt?: Date,
   ) {}
 }
