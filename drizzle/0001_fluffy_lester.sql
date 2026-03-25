7424f63375d2ad0bcf18beeb415c71f24f3a815e
deiz0n <eduardomacedosilva@outlook.com>
2026-03-25 00:16:44 -0300

    fix: o id do médico agora é uuid

---
+++ drizzle/0001_fluffy_lester.sql
@@ -0,0 +1,2 @@
+ALTER TABLE "tb_medico" ALTER COLUMN "id" SET DATA TYPE uuid;--> statement-breakpoint
+ALTER TABLE "tb_medico" ALTER COLUMN "id" SET DEFAULT gen_random_uuid();
--- src/infrastructure/adapters/persistence/MedicoSchema.ts
+++ src/infrastructure/adapters/persistence/MedicoSchema.ts
@@ -1,7 +1,7 @@
-import { pgTable, serial, timestamp, varchar } from 'drizzle-orm/pg-core';
+import { pgTable, uuid, timestamp, varchar } from 'drizzle-orm/pg-core';
 
 export const medicoSchema = pgTable('tb_medico', {
-  id: serial('id').primaryKey(),
+  id: uuid('id').primaryKey().defaultRandom(),
 
   nome: varchar('nome').notNull(),
 
